28616af6c637c66ff92b56b99bebdbeedb5064d5
testaccount-account <89683885+testaccount-account@users.noreply.github.com>
2021-10-01 14:27:43 +0200

    Create decimal_to_binary.py

---
+++ Python/decimal_to_binary.py
@@ -0,0 +1,14 @@
+
+
+def decimal_to_binary(decimal_number: int) -> str:
+    """Function to convert a decimal number to a binary number."""
+    binary = ""
+    while decimal_number // 2 > 0:
+        binary += str(decimal_number % 2)
+        decimal_number = decimal_number // 2
+    binary += str(decimal_number % 2)
+
+    return binary[::-1]
+
+
+print(decimal_to_binary(10))  # 1010
